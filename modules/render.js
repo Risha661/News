fa3c9eb5c4b1cc2f2e5752e480fc6741b2af61b8
unknown <git@github.com:Risha661/CRM.git>
2024-06-11 16:45:13 +0300

    test2

--- modules/render.js
+++ modules/render.js
@@ -13,6 +13,7 @@ const newsList = document.querySelectorAll('.news-list');
 const newsSection = document.querySelector('.news');
 const renderWrapper = document.querySelector('.title-wrapper');
 const textWrapper = renderWrapper.querySelector('.title');
+const searchQuery = document.querySelector('.search-input').value;
 
 // newsSection.querySelector('.news-list').innerHTML = '';
 const displayNews = (articles) => {
@@ -41,25 +42,77 @@ const displayNews = (articles) => {
 
 textWrapper.textContent = 'Введите ваш запрос';
 
-const renderNews = () => {
 
-  searchButton.addEventListener('click', async () => { 
-    const searchQuery = document.querySelector('.search-input').value;
-    try {
-      const response = await fetch('https://newsapi.org/v2/top-headlines?country=ru', {
-        headers: {
-          'X-Api-Key': '45896303d3c54e8d89e7a9ffc68f4091'
-        },
-      });
-      console.log(response);
-      const data = await response.json();
+document.querySelector('.search-submit').addEventListener('click', (event) =>{
+  event.preventDefault();
+
+  const searchQuery = document.querySelector('.search-input').value;
+
+  let url = `https://newsapi.org/v2/top-headlines?country=&q=${searchQuery}&pageSize=8&apiKey=7dbb00f1877742d9818640ad6eb49c62`;
+  console.log(url);
+  console.log(searchQuery);
+  alert('1');
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
       console.log(data);
-      displayNews(data.articles);
-    } catch (error) {
-      console.error('Произошла ошибка: ', error);
-    }
-    alert('тут');
-  });
-};
+      console.log(data.articles);
+    })
+    .catch((error) => {
+      console.log('Произошла ошибка: ', error);
+    });
+});
 
-renderNews();
+
+// const searchButton = document.querySelector('.search-button');
+// searchButton.addEventListener('click', async () => { 
+//     
+//     const url = `https://newsapi.org/v2/top-headlines?country=ru${searchQuery}`;
+//     const key = '7dbb00f1877742d9818640ad6eb49c62';
+//     alert('1');
+//     try {
+//         const response = await fetch(url, {
+//             headers: {
+//                 'X-Api-Key': key
+//             },
+//         });
+
+//         alert('2');
+//         if (!response.ok) {
+//             throw new Error(`Ошибка: ${response.status}`);
+//         }
+//         const data = await response.json();
+//         console.log(data);
+//         displayNews(data.articles);
+//     } catch (error) {
+//       alert('3');
+//         console.error('Произошла ошибка: ', error);
+//     }
+// });
+
+
+// const renderNews = () => {
+
+//   searchButton.addEventListener('click', async () => { 
+//     const searchQuery = document.querySelector('.search-input').value;
+//     try {
+//       const response = await fetch('https://newsapi.org/v2/top-headlines?country=ru', {
+//         headers: {
+//           'X-Api-Key': '45896303d3c54e8d89e7a9ffc68f4091'
+//         },
+//       });
+//       console.log(response);
+//       const data = await response.json();
+//       console.log(data);
+//       displayNews(data.articles);
+//     } catch (error) {
+//       console.error('Произошла ошибка: ', error);
+//     }
+//     alert('тут');
+//   });
+// };
+
+// renderNews();
